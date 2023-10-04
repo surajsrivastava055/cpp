50c77d8ce843cd223d4b1d8a57d71cbc130c0611
Gyan Chandra shrivastava <94687923+surajsrivastav044@users.noreply.github.com>
2023-10-04 08:25:50 +0000

    hello

--- Problems/Day3/ConstDemo/Demo1.cpp
+++ Problems/Day3/ConstDemo/Demo1.cpp
@@ -1,22 +1,14 @@
 #include<iostream>
 #include "/workspaces/cpp/Problems/Day3/ScenarioD/Vehicle.h"
 
-
-
 // use case 1: with varibles of primitive types which are not pointers
 
 void Demo(){
-
-
     const  int n1=10;// n1 is now immutable, it can't be changed once intialized
     const char grade ='A';
     std::cout<<n1<< "\t"<< grade<<std::endl;
     //n1=20; // error
-
-
 }
 int main(){
-
-Demo();
-
+    Demo();
 }
